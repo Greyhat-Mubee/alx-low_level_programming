302cfd05c12638950b6a76c5f0efaa87b68c0f12
Greyhat-Mubee <ferukebellomubarak@aol.com>
2023-02-20 17:20:50 -0800

    adding print.c source code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * using printf to print to standard output
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
